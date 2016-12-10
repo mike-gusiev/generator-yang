e04eaf142918cb5ae5008c4fee6697cbaac6e7fa
mikeg <mike.gusiev@omnigon.com>
2016-12-11 01:42:18 +0200

    Unit tests structure

---
+++ test/test-app.js
@@ -0,0 +1,20 @@
+'use strict';
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+
+
+describe('yang:app', function (){
+
+    describe('default', function (){
+
+        before(function (){
+
+        });
+
+        it('creates files', function (){
+
+        });
+    });
+
+});
